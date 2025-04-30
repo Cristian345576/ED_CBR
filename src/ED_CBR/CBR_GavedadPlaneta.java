3b6868280105ec47f4fa3157cafeaa75ae253541
Barquero Rodríguez <cristian.barquero@isepceu.es>
2025-04-30 19:43:31 +0200

    Cbr_commit de la segunda clase GravedadPlaneta

---
+++ src/ED_CBR/CBR_GavedadPlaneta.java
@@ -0,0 +1,30 @@
+package ED_CBR;
+//cnjasdhad
+public class CBR_GavedadPlaneta {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getGravedadPlaneta() {
+		return GravedadPlaneta;
+	}
+	public void setGravedadPlaneta(double gravedadPlaneta) {
+		GravedadPlaneta = gravedadPlaneta;
+	}
+	@Override
+	public String toString() {
+		return "CBR_GavedadPlaneta [id=" + id + ", nombre=" + nombre + ", GravedadPlaneta=" + GravedadPlaneta + "]";
+	}
+	private int id;
+	private String nombre;
+	private double GravedadPlaneta;
+
+}
